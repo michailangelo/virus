a4e20e10f952f54111b8c984071243c5ddf2a8c0
Michail Samartzis <michailangelo@gmail.com>
2020-05-19 01:47:30 +0300

    The first commit.

---
+++ src/main/java/com/project/virus/VirusApplication.java
@@ -0,0 +1,13 @@
+package com.project.virus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/virus/VirusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.virus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
