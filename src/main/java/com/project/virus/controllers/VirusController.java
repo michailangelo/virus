e52d520298a438105c072749ffafec911d7789c4
John Matzakos <ioannis.matzakos@gmail.com>
2020-05-22 11:28:27 +0300

    Updated controller

--- src/main/java/com/project/virus/controllers/VirusController.java
+++ src/main/java/com/project/virus/controllers/VirusController.java
@@ -2,16 +2,40 @@
 
 package com.project.virus.controllers;
 
+import com.project.virus.models.Covid19Data;
+import com.project.virus.services.VirusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class VirusController {
 
+    @Autowired
+    VirusService virusService;
+
     @GetMapping("/")
     public String index(Model model){
+
+        List<Covid19Data> dataList = virusService.getDataList();
+
+        for(Covid19Data i : dataList) {
+            model.addAttribute("country", i.getCountry());
+            model.addAttribute("region", i.getRegion());
+            model.addAttribute("county", i.getCounty());
+            model.addAttribute("confirmedCases", i.getConfirmedCases());
+            model.addAttribute("deaths", i.getDeaths());
+            model.addAttribute("hospitalized", i.getHospitalized());
+            model.addAttribute("criticalCases", i.getCriticalCases());
+            model.addAttribute("recoveredCases", i.getRecoveredCases());
+        }
+
         return "index";
     }
+
+
 
 }
